db252fcc52c8bab1ff43555323a14dffc334b5b9
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2022-05-15 12:50:15 -0500

    avance domingo mañana

--- lib/screens/products_menu.dart
+++ lib/screens/products_menu.dart
@@ -12,60 +12,89 @@ class ProductsMenu extends StatefulWidget {
 }
 
 class _ProductsMenuState extends State<ProductsMenu> {
+  List products = [
+    {
+      "name": "Abuta",
+      "type": "Analgésico",
+      "img": "https://static.inaturalist.org/photos/37321715/large.jpg"
+    },
+    {
+      "name": "Ajo sacha",
+      "type": "Antiglucémico",
+      "img":
+          "https://delamazonas.com/wp-content/uploads/2021/05/Garlic-Vine-Mansoa-alliacea-aka-Ajos-Sacha-in-Kerala-low.jpg"
+    },
+    {
+      "name": "Copaiba",
+      "type": "Cicatrizante",
+      "img":
+          "https://www.pebani.com.pe/wp-content/uploads/2018/05/Copaiba_frutos1.png"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-        return Card(
-            // shape:
-            //     RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Column(
-              children: [
-                SizedBox(
-                  child: GestureDetector(
-                      onTap: () {
-                        _showUnitProductMenu(context);
-                        Colors.amber;
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            // padding: EdgeInsets.symmetric(vertical: 20),
-                            // margin: EdgeInsets.all(15),
-                            child: Image.network(
-                              "https://static.inaturalist.org/photos/37321715/large.jpg",
-                              fit: BoxFit.cover,
-                              height: 250,
-                              width: 450,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, int index) {
+            // final product = products[index];
+            return Card(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 310,
+                      child: GestureDetector(
+                          onTap: () {
+                            _showUnitProductMenu(context);
+                          },
+                          child: Column(
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 10)),
-                                  Text("Abuta", style: TextStyle(fontSize: 18)),
-                                  Text('Analgésico'),
-                                ],
+                              Container(
+                                child: Image.network(
+                                  "https://static.inaturalist.org/photos/37321715/large.jpg",
+                                  fit: BoxFit.cover,
+                                  height: 250,
+                                  width: 450,
+                                ),
                               ),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: Container(
-                                    child: Text("Ver detalles"),
-                                  ))
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Abuta",
+                                            style: TextStyle(fontSize: 18)),
+                                        Text('Analgésico'),
+                                      ],
+                                    ),
+                                    TextButton(
+                                        onPressed: () {
+                                          _showUnitProductMenu(context);
+                                        },
+                                        child: Text("Ver opciones"))
+                                  ],
+                                ),
+                              ),
+                              // SizedBox(
+                              //   width: 330,
+                              //   height: 60,
+                              //   child:
+                              // )
                             ],
-                          )
-                        ],
-                      )),
-                )
-              ],
-            ));
-      }),
+                          )),
+                    )
+                  ],
+                ));
+          }),
     );
   }
 }
