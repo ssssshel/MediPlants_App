23200c2f7f02a77a871ce972357541fd17ddf94c
ssssshel <angelarteaga989@gmail.com>
2022-05-16 14:17:18 -0500

    stsh

--- lib/pages/principal.dart
+++ lib/pages/principal.dart
@@ -4,6 +4,9 @@ import '../screens/home_page.dart';
 import '../screens/products_menu.dart';
 import '../screens/user_profile.dart';
 
+// import '../other_branches/products_menu_carlos.dart';
+// import '../other_branches/productsbagkeiko.dart';
+
 import "../components/appbar.dart";
 import '../components/productsbag.dart';
 
